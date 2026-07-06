be4b964b07d1d5f72776d05f8b33ec89e7d7288a
IgnacioLapko <ilapko@frba.utn.edu.ar>
2026-07-06 17:11:09 -0300

    Agregado de repository

---
+++ src/main/java/ar/edu/utn/ba/ddsi/climalert/repositories/impl/RegistroClimaRepositoryImpl.java
@@ -0,0 +1,29 @@
+package ar.edu.utn.ba.ddsi.climalert.repositories.impl;
+
+import ar.edu.utn.ba.ddsi.climalert.models.RegistroClima;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Repository
+public class RegistroClimaRepositoryImpl {
+
+  private final List<RegistroClima> almacenamientoEnMemoria = new CopyOnWriteArrayList<>();
+
+  public void save(RegistroClima registro) {
+    almacenamientoEnMemoria.add(registro);
+  }
+
+  public Optional<RegistroClima> findLast() {
+    if (almacenamientoEnMemoria.isEmpty()) {
+      return Optional.empty();
+    }
+    return Optional.of(almacenamientoEnMemoria.get(almacenamientoEnMemoria.size() - 1));
+  }
+
+  public List<RegistroClima> findAll() {
+    return almacenamientoEnMemoria;
+  }
+}
